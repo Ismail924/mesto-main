e612bbf1548ebc71945d5afe1de27b916acd209b
Ismail <ismailuzbaev@yandex.ru>
2023-06-17 15:12:49 +0300

    mesto

---
+++ script.js
@@ -0,0 +1,29 @@
+const popUp = document .querySelector('.popap');
+const openPopUp = document.querySelector('.profile__edit-button');
+const closePopUp = document.querySelector('.popap__close');
+let formElement = document.querySelector('.form');
+let profileName = document.querySelector('.profile__name');
+let profileactivity = document.querySelector('.profile__position');
+let popUpName = document.querySelector('.form__input_value_name');
+let popUpActivity = document.querySelector('.form__input_value_position');
+
+openPopUp.addEventListener("click", () => {
+  popUp.classList.add('popap_opened');
+  popUpName.value = profileName.textContent;
+  popUpActivity.value = profileactivity.textContent;
+
+})
+
+closePopUp.addEventListener('click', () => {
+popUp.classList.remove('popap_opened');
+
+})
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = popUpName.value;
+  profileactivity.textContent = popUpActivity.value;
+  popUp.classList.remove('popap_opened');
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
